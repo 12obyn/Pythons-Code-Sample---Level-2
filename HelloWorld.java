a29bcc4fa03659cd353637afb10e08cbc9baefcb
Maylene Garcia <47190324+maylene128@users.noreply.github.com>
2019-05-07 16:00:32 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+ // Maylene Garcia
+ // 5-7-19
+ 
+ /* Module 5 Problem 1
+    Prints Hello World*/
+	
+	
+class HelloWorld
+{
+	public static void main(String []args)
+	{
+		System.out.println ("Hello World");
+	}
+}
---
+++ problem2.java
@@ -0,0 +1,12 @@
+ // Maylene Garcia
+ // 5-7-19
+ 
+ class problem2
+ {
+	public static void main(String []args)
+	{
+		int sampleNumber = 5;
+		
+		System.out.println(sampleNumber);
+	}
+}
